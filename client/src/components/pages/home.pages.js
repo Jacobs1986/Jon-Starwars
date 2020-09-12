25e2a82d2dceb9eded69c6fde603ea5189b2c2d9
Sean Jacobson <sean.jacobson86@gmail.com>
2020-09-12 15:42:33 -0600

    Added images to page

--- client/src/components/pages/home.pages.js
+++ client/src/components/pages/home.pages.js
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
 
  // Bootstrap
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Card, CardColumns } from "react-bootstrap";
 
 class Home extends Component {
+    state= {
+        images: ["page1", "page2", "page3", "page4"]
+    }
+    
     render() {
         return (
             <div>
@@ -11,9 +15,13 @@ class Home extends Component {
                     <h1>Starwars Comic by Jonathan Morales</h1>
                     <p>Disclaimer: This is a fan made comic. I do not own or make any money from this comic. All characters are owned by Disney.</p>
                     <Row>
-                        <Col md={12}>
-                            
-                        </Col>
+                        <CardColumns>
+                            {this.state.images.map((info, index) => (
+                                <Card key={index}>
+                                    <Card.Img src={require(`./Images/${info}.JPG`)} />
+                                </Card>
+                            ))}
+                        </CardColumns>
                     </Row>
                 </Container>
             </div>
