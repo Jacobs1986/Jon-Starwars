821e3af2df65408ba5c464cd0c5d2dd1a3ff48c3
Sean Jacobson <sean.jacobson86@gmail.com>
2020-09-12 14:58:14 -0600

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,9 @@ import { BrowserRouter as Router, Route } from "react-router-dom"
 // Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 
+// Styling
+import "./App.css"
+
 class App extends Component {
     render() {
         return (
