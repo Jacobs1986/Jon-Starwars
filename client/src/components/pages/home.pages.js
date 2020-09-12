9138f35b198e1d81c09143c2caac5c43b7009527
Sean Jacobson <sean.jacobson86@gmail.com>
2020-09-12 15:05:44 -0600

    Update home.pages.js

---
+++ client/src/components/pages/home.pages.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+ // Bootstrap
+import { Container, Row, Col } from "react-bootstrap";
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                This is the home page
+            </div>
+        );
+    };
+}
+
+export default Home
