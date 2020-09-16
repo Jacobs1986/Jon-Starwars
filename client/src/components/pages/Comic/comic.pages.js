3324203c6230ae545f5fcc18f4abdd9938320303
Sean Jacobson <sean.jacobson86@gmail.com>
2020-09-16 17:20:49 -0600

    Update comic.pages.js
    I added the images to the page

--- client/src/components/pages/Comic/comic.pages.js
+++ client/src/components/pages/Comic/comic.pages.js
@@ -1,13 +1,27 @@
 import React, { Component } from "react";
 
  // Bootstrap
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Image } from "react-bootstrap";
 
 class Chapter extends Component {
+    state = {
+        images: ["page-1", "page-2", "page-3", "page-4"],
+        title: "Chapter 1"
+    }
+
     render() {
         return (
             <div>
-                This is a reactjs page.
+                <Container>
+                    <h1 style={{ textAlign: 'center' }}>{this.state.title}</h1>
+                    {this.state.images.map((image, index) => (
+                        <Row>
+                            <Col md={12}>
+                                <Image src={require(`../Images/chapter-1/${image}.JPG`)} fluid/>
+                            </Col>
+                        </Row>
+                    ))}
+                </Container>
             </div>
         );
     };
