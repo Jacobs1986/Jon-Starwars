0fa4da53742adf73091cf10651bdee0025baac06
Sean Jacobson <sean.jacobson86@gmail.com>
2020-09-16 17:14:21 -0600

    Created comic page
    This is where the user can view the different chapters

---
+++ client/src/components/pages/Comic/comic.pages.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+ // Bootstrap
+import { Container, Row, Col } from "react-bootstrap";
+
+class Chapter extends Component {
+    render() {
+        return (
+            <div>
+                This is a reactjs page.
+            </div>
+        );
+    };
+}
+
+export default Chapter
