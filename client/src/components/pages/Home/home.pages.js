789ef2420e5e81fc3b0e20a804ec4d358698b270
Sean Jacobson <sean.jacobson86@gmail.com>
2020-09-17 13:17:55 -0600

    Update home.pages.js

--- client/src/components/pages/Home/home.pages.js
+++ client/src/components/pages/Home/home.pages.js
@@ -10,7 +10,9 @@ class Home extends Component {
                 <Container>
                     <h1 className="center">The Rise of Skywalker AU</h1>
                     <h3 className="center">A fan comic by Jonathan Morales</h3>
-                    <p>Disclaimer: This is a fan made comic. I do not own or make any money from this comic. All characters are owned by Disney.</p>
+                    <p>DDisclaimer:
+                    This site contains copyrighted material the use of which has not always been specifically authorized by the copyright owner. In accord with our non-profit mission we are making such material available to fans of Star Wars in parody form in efforts to entertain such fans for free for the love of Star Wars. We believe this constitutes a “fair use” of any such copyrighted material as provided in Section 107 of the US copyright Law. In accordance with Title 17 U.S.C. Section 107, The material of this site is distributed without profit to those who have expressed a prior interest in receiving the included content.
+                    This is a fan made comic. I do not make any money from this comic. All characters are owned by Disney.</p>
                     <Row>
                         <CardColumns>
                             <Card style={{ width: '18rem' }} bg="dark">
