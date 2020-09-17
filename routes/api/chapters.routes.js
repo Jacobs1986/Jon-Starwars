2ffaa70917a65b0c4e5370fae650251402475a1c
Sean Jacobson <sean.jacobson86@gmail.com>
2020-09-17 12:00:05 -0600

    Update chapters.routes.js
    Created a testing route

--- routes/api/chapters.routes.js
+++ routes/api/chapters.routes.js
@@ -1,3 +1,9 @@
 const router = require("express").Router();
 
+// Chapter one, path /api/chapters/chapter1
+router.route("/chapter1")
+    .get((requrest, response) => {
+        response.json("The route is working.")
+    })
+
 module.exports = router;
