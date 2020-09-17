f9387d16cf705d5ca36d94ee840d89eff5e5b537
Sean Jacobson <sean.jacobson86@gmail.com>
2020-09-17 13:38:01 -0600

    Update home.pages.js

--- client/src/components/pages/Home/home.pages.js
+++ client/src/components/pages/Home/home.pages.js
@@ -8,7 +8,7 @@ class Home extends Component {
         return (
             <div>
                 <Container>
-                    <h1 className="center font-holotwood">The Rise of Skywalker AU</h1>
+                    <h1 className="center font-holtwood">The Rise of Skywalker AU</h1>
                     <h3 className="center font-holtwood">A fan comic by Jonathan Morales</h3>
                     <p>Disclaimer:
                     This site contains copyrighted material the use of which has not always been specifically authorized by the copyright owner. In accord with our non-profit mission we are making such material available to fans of Star Wars in parody form in efforts to entertain such fans for free for the love of Star Wars. We believe this constitutes a “fair use” of any such copyrighted material as provided in Section 107 of the US copyright Law. In accordance with Title 17 U.S.C. Section 107, The material of this site is distributed without profit to those who have expressed a prior interest in receiving the included content.
