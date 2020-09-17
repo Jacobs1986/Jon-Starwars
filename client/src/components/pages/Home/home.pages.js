4526cbd839dcc7b7cad409381e21f7c8eba03709
Sean Jacobson <sean.jacobson86@gmail.com>
2020-09-17 12:23:24 -0600

    Revert "Update home.pages.js"
    This reverts commit ece70eb9de70b8b5b4f5cdc594eb72daf4794db0.

--- client/src/components/pages/Home/home.pages.js
+++ client/src/components/pages/Home/home.pages.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-// Bootstrap
+ // Bootstrap
 import { Container, Row, Card } from "react-bootstrap";
 
 class Home extends Component {
@@ -10,9 +10,9 @@ class Home extends Component {
                 <Container>
                     <h1 className="center font-holtwood">The Rise of Skywalker AU</h1>
                     <h3 className="center font-holtwood">A fan comic by Jonathan Morales</h3>
-                    <p>Disclaimer: This site contains copyrighted material the use of which has not always been specifically authorized by the copyright owner. In accord with our non-profit mission we are making such material available to fans of Star Wars in parody form in efforts to entertain such fans for free for the love of Star Wars. We believe this constitutes a “fair use” of any such copyrighted material as provided in Section 107 of the US copyright Law. In accordance with Title 17 U.S.C. Section 107, The material of this site is distributed without profit to those who have expressed a prior interest in receiving the included content. This is a fan made comic. I do not make any money from this comic. All characters are owned by Disney.</p>
+                    <p>Disclaimer: This is a fan made comic. I do not own or make any money from this comic. All characters are owned by Disney.</p>
                     <Row>
-                        <Card style={{ width: '18rem' }} bg="dark">
+                        <Card style={{ width: '18rem'}} bg="dark">
                             <a href="/chapter?chapter1"><Card.Img src={require("../Images/chapter-1/page-1.JPG")} /></a>
                             <Card.Body>
                                 <Card.Title style={{ textAlign: 'center' }}>Chapter 1</Card.Title>
