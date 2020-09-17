7af348395d8c90a8e926e31cc6820d7c40b7b729
Sean Jacobson <sean.jacobson86@gmail.com>
2020-09-17 13:03:57 -0600

    Update home.pages.js

--- client/src/components/pages/Home/home.pages.js
+++ client/src/components/pages/Home/home.pages.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
- // Bootstrap
-import { Container, Row, Card } from "react-bootstrap";
+// Bootstrap
+import { Container, Row, Card, CardColumns } from "react-bootstrap";
 
 class Home extends Component {
     render() {
@@ -12,12 +12,20 @@ class Home extends Component {
                     <h3 className="center">A fan comic by Jonathan Morales</h3>
                     <p>Disclaimer: This is a fan made comic. I do not own or make any money from this comic. All characters are owned by Disney.</p>
                     <Row>
-                        <Card style={{ width: '18rem'}} bg="dark">
-                            <a href="/chapter?chapter1"><Card.Img src={require("../Images/chapter-1/page-1.JPG")} /></a>
-                            <Card.Body>
-                                <Card.Title style={{ textAlign: 'center' }}>Chapter 1</Card.Title>
-                            </Card.Body>
-                        </Card>
+                        <CardColumns>
+                            <Card style={{ width: '18rem' }} bg="dark">
+                                <a href="/chapter?chapter1"><Card.Img src={require("../Images/chapter-1/page-1.JPG")} /></a>
+                                <Card.Body>
+                                    <Card.Title style={{ textAlign: 'center' }}>Chapter 1</Card.Title>
+                                </Card.Body>
+                            </Card>
+                            <Card style={{ width: '20rem' }} bg="dark">
+                                <a href="/chapter?chapter2"><Card.Img src={require("../Images/chapter-2/page-1.JPG")} /></a>
+                                <Card.Body>
+                                    <Card.Title style={{ textAlign: 'center' }}>Chapter 2</Card.Title>
+                                </Card.Body>
+                            </Card>
+                        </CardColumns>
                     </Row>
                 </Container>
             </div>
