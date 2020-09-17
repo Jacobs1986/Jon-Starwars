25164e1c0cb13509722fc9030f78cf2655283d9d
Sean Jacobson <sean.jacobson86@gmail.com>
2020-09-17 11:48:04 -0600

    Update server.js
    Added the routes to the server file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const routes = require('./routes');
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -11,10 +11,8 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
 
-// Define API routes here
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// Routes
+app.use("/", routes);
 
 app.listen(PORT, () => {
     console.log("🌎 ==> API server now on port " + PORT);
