7a8c3fb57024a40799847369f1a4abd648fa2c63
Sean Jacobson <sean.jacobson86@gmail.com>
2020-09-17 12:11:07 -0600

    Update chapters.routes.js

--- routes/api/chapters.routes.js
+++ routes/api/chapters.routes.js
@@ -1,9 +1,13 @@
 const router = require("express").Router();
+const folderChapter1 = "./chapter-1/";
+const fs = require('fs');
 
 // Chapter one, path /api/chapters/chapter1
 router.route("/chapter1")
     .get((requrest, response) => {
-        response.json("The route is working.")
+        fs.readdirSync(folderChapter1).forEach(file => {
+            console.log(file);
+        })
     })
 
 module.exports = router;
