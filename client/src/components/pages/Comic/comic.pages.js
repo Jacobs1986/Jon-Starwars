641ab43fdd93ed060404e809bbfd985af99433b2
Sean Jacobson <sean.jacobson86@gmail.com>
2020-09-17 21:43:15 -0600

    Update comic.pages.js

--- client/src/components/pages/Comic/comic.pages.js
+++ client/src/components/pages/Comic/comic.pages.js
@@ -60,7 +60,7 @@ class Chapter extends Component {
                 this.setState({
                     title: "Chapter 5",
                     folder: "chapter-5",
-                    images: ["page-1", "page-2", "page-3", "page-4", "page-5", "page-6", "page-7", "page-8", "page-9", "page-10", "page-11", "page-12", "page-13", "page-14", "page-15", "page-16", "page-17", "page-18", "page-19", "page-20", "page-21", "page-22", "page-23", "page-24", "page-25", "page-26", "page-27", "page-28", "page-29", "page-30", "page-31", "page-31", "page-33", "page-34", "page-35"],
+                    images: ["page-1", "page-2", "page-3", "page-4", "page-5", "page-6", "page-7", "page-8", "page-9", "page-10", "page-11", "page-12", "page-13", "page-14", "page-15", "page-16", "page-17", "page-18", "page-19", "page-20", "page-21", "page-22", "page-23", "page-24", "page-25", "page-26", "page-27", "page-28", "page-29", "page-30", "page-31", "page-31", "page-33", "page-34", "page-35", "page-36"],
                     video1: "chapter-5a",
                     video2: "chapter-5b",
                     video3: "chapter-5c"
