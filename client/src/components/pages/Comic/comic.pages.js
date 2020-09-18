f273d147682802fa034a741dbed2bc731429fce4
Sean Jacobson <sean.jacobson86@gmail.com>
2020-09-17 21:17:15 -0600

    Update comic.pages.js

--- client/src/components/pages/Comic/comic.pages.js
+++ client/src/components/pages/Comic/comic.pages.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
- // Bootstrap
+// Bootstrap
 import { Container, Row, Col, Image } from "react-bootstrap";
 
 // styling
@@ -21,12 +21,12 @@ class Chapter extends Component {
     loadInfo = () => {
         let url = window.location.search;
         let chapter = url.split("?")[1];
-        switch(chapter) {
+        switch (chapter) {
             case "chapter1":
                 this.setState({
                     title: "Chapter 1",
                     folder: "chapter-1",
-                    images: ["page-1", "page-2", "page-3", "page-4", "page-5", "page-6", "page-7", "page-8", "page-9", "page-10","page-11", "page-12", "page-13", "page-14", "page-15", "page-16", "page-17", "page-18", "page-19", "page-20", "page-21", "page-22", "page-23", "page-24", "page-25", "page-26", "page-27", "page-28", "page-29", "page-30", "page-31", "page-31", "page-33", "page-34", "page-35", "page-36", "page-37", "page-38", "page-39", "page-40"]
+                    images: ["page-1", "page-2", "page-3", "page-4", "page-5", "page-6", "page-7", "page-8", "page-9", "page-10", "page-11", "page-12", "page-13", "page-14", "page-15", "page-16", "page-17", "page-18", "page-19", "page-20", "page-21", "page-22", "page-23", "page-24", "page-25", "page-26", "page-27", "page-28", "page-29", "page-30", "page-31", "page-31", "page-33", "page-34", "page-35", "page-36", "page-37", "page-38", "page-39", "page-40"]
                 })
                 break
             case "chapter2":
@@ -45,15 +45,23 @@ class Chapter extends Component {
                     video: "chapter-3"
                 })
                 break
-                case "chapter4":
-                    this.setState({
-                        title: "Chapter 4",
-                        folder: "chapter-4",
-                        images: ["page-1", "page-2", "page-3", "page-4", "page-5", "page-6", "page-7", "page-8", "page-9", "page-10","page-11", "page-12", "page-13", "page-14", "page-15", "page-16", "page-17", "page-18", "page-19", "page-20", "page-21", "page-22", "page-23", "page-24", "page-25", "page-26", "page-27", "page-28", "page-29"],
-                        video: "chapter-4"
-                    })
-                    break
-            default: 
+            case "chapter4":
+                this.setState({
+                    title: "Chapter 4",
+                    folder: "chapter-4",
+                    images: ["page-1", "page-2", "page-3", "page-4", "page-5", "page-6", "page-7", "page-8", "page-9", "page-10", "page-11", "page-12", "page-13", "page-14", "page-15", "page-16", "page-17", "page-18", "page-19", "page-20", "page-21", "page-22", "page-23", "page-24", "page-25", "page-26", "page-27", "page-28", "page-29"],
+                    video: "chapter-4"
+                })
+                break
+            case "chapter5":
+                this.setState({
+                    title: "Chapter 5",
+                    folder: "chapter-5",
+                    images: ["page-1", "page-2", "page-3", "page-4", "page-5", "page-6", "page-7", "page-8", "page-9", "page-10", "page-11", "page-12", "page-13", "page-14", "page-15", "page-16", "page-17", "page-18", "page-19", "page-20", "page-21", "page-22", "page-23", "page-24", "page-25", "page-26", "page-27", "page-28", "page-29", "page-30", "page-31", "page-31", "page-33", "page-34", "page-35"],
+                    video: "chapter-5a"
+                })
+                break
+            default:
         }
     }
 
@@ -72,7 +80,7 @@ class Chapter extends Component {
                     {this.state.images.map((image, index) => (
                         <Row key={index}>
                             <Col md={12}>
-                                <Image src={require(`../Images/${this.state.folder}/${image}.JPG`)} fluid/>
+                                <Image src={require(`../Images/${this.state.folder}/${image}.JPG`)} fluid />
                             </Col>
                         </Row>
                     ))}
